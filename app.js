c4f61b32698cce7bf7f51748935f20c094a263a5
shahdmagdi1 <shahd227819@bue.edu.eg>
2024-12-05 12:47:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,17 @@
-/*const express = require('express');
-const dotenv = require('dotenv');
-const locationRoutes = require('./routes/locationRoutes'); // Location routes
-const usersRouter = require('./routes/usersRoutes'); // User routes
-const initiateDBConnection = require('./config/db'); // Database connection
-
-dotenv.config({
-    path: './config/.env',
-});
-
-const app = express();
-const PORT = process.env.PORT || 3000; // Default to port 3000 if not defined
-
-// Middleware
-app.use(express.json()); // Parse JSON requests
-
-// Routes
-app.use('/users', usersRouter); // User routes
-app.use('/api/locations', locationRoutes); // Location routes
-
-// Start server
-app.listen(PORT, async () => {
-    console.log(`Server has been started and is listening on port ${PORT}`);
-    await initiateDBConnection();
-});
-
-module.exports = app;*/
-
-
 const express = require('express');
 const dotenv = require('dotenv');
 const locationRoutes = require('./routes/locationRoutes'); // Location routes
 const usersRouter = require('./routes/usersRoutes'); // User routes
-const tripRoutes = require('./routes/tripRoutes'); // Trip routes
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const singleServicesRoutes = require('./routes/singleServicesRoutes'); // Import single services routes
+const initiateDBConnection = require('./config/db'); // Database connection
+const publicTransportationRoutes = require('./routes/publicTransportationRoutes');
 
 dotenv.config({
     path: './config/.env',
 });
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Default to port 5000 if not defined
+const PORT = process.env.PORT || 5000; // Default to port 3000 if not defined
 
 // Middleware
 app.use(express.json()); // Parse JSON requests
@@ -49,20 +19,11 @@ app.use(express.json()); // Parse JSON requests
 // Routes
 app.use('/users', usersRouter); // User routes
 app.use('/api/locations', locationRoutes); // Location routes
-app.use('/api/trips', tripRoutes); // Trip routes
+app.use('/api/singleservices', singleServicesRoutes);
+app.use('/api/publictransportation', publicTransportationRoutes);
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_CONNECTION_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log('Database connection error:', err));
-
-// Start server
+// Start server and initialize DB
 app.listen(PORT, async () => {
     console.log(`Server has been started and is listening on port ${PORT}`);
+    await initiateDBConnection();
 });
-
-module.exports = app;
-
